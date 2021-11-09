a5c095e93f53458560b04604a9512971c9ba17c0
Qohat <qohatpp@gmail.com>
2021-11-08 23:34:33 -0500

    List exercises

--- src/List.hs
+++ src/List.hs
@@ -1,4 +1,5 @@
 module List where
+import GHC.Base (Ord)
 
 f :: Ord a => [a] -> [a]
 f [] = []
@@ -11,4 +12,44 @@ firstItem :: Ord a => [a] -> a
 firstItem = head
 
 lastItem :: Ord a => [a] -> a
-lastItem = tail
+lastItem = last
+
+nth :: Ord a => [a] -> a
+nth xs = xs !! 3
+
+lastItems :: Ord a => [a] -> [a]
+lastItems = tail
+
+factorial :: (Num a, Enum a) => a -> a
+factorial n = product [1..n]
+
+average :: Foldable t => t Int -> Int
+average ns = sum ns `div` length ns
+{- average ns = div (sum ns) (length ns) -}
+{- x `f` y is synctatic sugar of f x y-}
+
+
+{- Find the syntax error
+N = a 'div' length xs
+    where 
+        a = 10
+        xs = [1, 2, 3, 4, 5, 6] -}
+
+n :: Int
+n = a `div` length xs
+    where 
+        a = 1
+        xs = [1, 2, 3, 4, 5, 6]
+
+last1 :: Ord a => [a] -> a
+last1 ls = ls !! (length ls - 1)
+
+last2 :: [a] -> a
+last2 ls = head (reverse ls)
+
+init1 :: [a] -> [a]
+init1 ls = reverse (tail (reverse ls))
+
+init2 :: [a] -> [a]
+init2 ls = take (length ls - 1) ls
+
